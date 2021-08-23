ebe76cf3ad0eb4726e0e6b11c2b1cb1b84d4bb47
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-23 13:16:10 +0300

    Update commands.js

--- commands/public/commands.js
+++ commands/public/commands.js
@@ -12,43 +12,11 @@ module.exports = {
 
   const embed = new Discord.MessageEmbed()
   .setAuthor(`${message.author.username}`, `${message.author.avatarURL({dynamic:true})}`)
-  .setDescription(`
-  **Public commands**
-
- \`avatar\`, \`corona\`, \`help\`, \`short\`, \`translate\`, \`user\`, \`ping\`, \`server\`, \`bot\`, \`ravatar\`
-
-  **🛠️ Moderation commands**
-
- \`addemoji\`, \`aliase\`, \`ban\`, \`clear\`, \`embed\`, \`hide\`, \`kick\`, \`lock\`, \`mute\`, \`role\`, \`setlang\`, \`setprefix\`, \`toogle\`, \`unhide\`, \`unlock\`, \`unmute\`, \`vkick\`, \`moveroles\`, \`transferroles\`, \`roles\` 
-
-**Protection commands**
-
- \`settings\`, \`antispam\`, \`whitelist-add\`, \`whitelist-delete\`, \`whitelist\`
-  
-**Ticket commands**
- 
- \`tr\`, \`add\`, \`remove\`, \`new\`, \`rename\`, \`close\`, \`close-all\`, \`ticket-role\`, \`ticket-number\`, \`ticket-toggle\`, \`ticket-description\`, \`ticket-config\`, \`ticket-category\`
- 
- **Fun commands**
-
- \`aki\`, \`emoji\`, \`fakebot\`, \`اسرع\`, \`فكك\`, \`hug\`, \`kill\`, \`kiss\`, \`kt\`, \`slap\`, \`points\`, \`trash\`, \`wanted\`, \`love\`
-  
- **Economy commands**
- 
- \`profile\`, \`coins\`, \`daily\`, \`rep\`, \`pay\`, \`title\`, \`background\`, \`background-remove\`
- 
- **Badwords commands**
-
- \`addword\`, \`delword\`, \`wordlist\`
-
- **Giveaway commands**
-
- \`gstart\`, \`gend\`, \`greroll\`, \`gsetrole\`, \`gblacklist\`, \`gcheck\`
- 
- **ReactionRoles**
-
- \`rradd\`, \`rrdel\`
- `)
+  .addField("🌐 Public Commands", `\`avatar\`, \`corona\`, \`help\`, \`short\`, \`translate\`, \`user\`, \`ping\`, \`server\`, \`bot\`, \`ravatar\``)
+  .addField("🛠️ Moderation Commands", `\`addemoji\`, \`ban\`, \`clear\`, \`embed\`, \`hide\`, \`kick\`, \`lock\`, \`mute\`, \`role\`, \`unhide\`, \`unlock\`, \`unmute\`, \`vkick\`, \`moveroles\`, \`transferroles\`, \`roles\``) 
+  .addField("😅 Fun Commands", `\`aki\`, \`emoji\`, \`fakebot\`, \`hug\`, \`kill\`, \`kiss\`, \`slap\`, \`trash\`, \`wanted\`, \`love\``)
+  .addField("💰 Economy Commands", `\`profile\`, \`coins\`, \`daily\`, \`rep\`, \`pay\`, \`title\`, \`background\`, \`background-remove\``)
+  .addField("🎁 Giveway Commands", `\`gstart\`, \`gend\`, \`greroll\`, \`gsetrole\`, \`gblacklist\`, \`gcheck\``)
   .setColor('9e1c36');
     
         message.channel.send(embed)
