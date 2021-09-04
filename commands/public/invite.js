0ba15543d240b406b060bb6bd8650cd13cfbc392
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-09-04 19:53:46 +0300

    Delete invite.js

--- commands/public/invite.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js')
-module.exports = {
-    name: "invite",
-    cooldown: 5,
-    run: async (client, message, args) => {
-      message.channel.send(`Bot is on updatr)
-    }}
