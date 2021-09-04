d929569b0223421f563c359beec5f5370219a365
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-09-04 13:47:50 +0300

    Update invite.js

--- commands/public/invite.js
+++ commands/public/invite.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 module.exports = {
-    name: "help",
+    name: "6",
     cooldown: 5,
     run: async (client, message, args) => {
       message.channel.send(new Discord.MessageEmbed().setColor("9e1c36").setDescription(`**For add me [Click here](https://discord.com/oauth2/authorize?client_id=803919676618833941&scope=bot&permissions=8)\n\n For support server [Click here](https://discord.gg/YE4JEphRAW)**`))
