8d66f247a9edc57d8904adb92e197f60738d7214
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-10 03:10:13 -0700

    Create girl.js

---
+++ commands/gif/girl.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { color } = require("../../config.json");
+const axios = require("axios");
+
+module.exports = {
+  name: "girl",
+  aliases: ["woman"],
+  description: "Gives Random Woman Gif/PP",
+  usage: "Gif",
+  run: async (client, message, args) => {
+
+        axios.get("https://api.roxza.me/v1/random?type=woman")
+            .then(res => {
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`**Woman Gif**`)
+                    .setColor(color)
+                    .setImage(res.data.url)
+                    .setFooter(`${message.author.tag}`, message.author.avatarURL)
+                   message.channel.send(embed);
+            })
+    }
+};
