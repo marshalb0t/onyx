90df4645610bb146fc8e7103a82456d78fefddbf
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-11 22:26:29 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -33,29 +33,20 @@ module.exports = (client, guild) => {
         button: [support,invite],
       });
 
- let channel = client.channels.cache.get("994335174005366785");
+ const channel = guild.channels.cache.find(c => c.type === "text" && c.permissionsFor(guild.me).has("CREATE_INSTANT_INVITE"));
+let inv;
+if(channel) { const invite = await channel.createInvite({temporary: false, maxAge: '0', maxUses: '0', unique: false, reason: 'FOR SECURITY REASONS!'}).catch(error => console.log(error))
+if(invite) { inv = invite.code;
+}
+}
 
- const joinEmbed = new MessageEmbed()
-       .setColor(client.color)
-       .setTitle("Joined A new Server!!")
-       .setThumbnail(guild.iconURL())
-       .addField(
-         "Guild:",
-         `i just joined to here **${guild.name}**.\n\nThere is **${guild.memberCount}** members`,
-         true
-       )
-       .addField(
-         "Owner:",
-         `${guild.owner.user.tag} / ${guild.ownerID}`,
-         true
-       )
-       .addField(
-         "Servers",
-         `[${client.guilds.cache.size}]`,
-         true
-       )
-       .setTimestamp() // moment().format('LLL'),
-       .setFooter(`Cool`);
-    channel.send(joinEmbed);
-
+    let chat = new Discord.MessageEmbed()
+    .setColor(Color)
+    .setURL(inv ? `https://discord.gg/${inv}` : null)
+    .setThumbnail(guild.iconURL({dynamic: true}))
+    .setFooter(`I Am In ${client.guilds.cache.size} Servers Now !`)
+    .setTitle(`Joined A Server !`)
+    .setDescription(`**Server Name**: ${guild.name}\n**Server ID**: ${guild.id}\n**Members**: ${guild.memberCount}\n**Owner**: ${guild.owner}\n**Owner ID**: ${guild.ownerID}`)
+    .setTimestamp();
+    client.channels.cache.get('994335071186194452').send(chat)
 }
