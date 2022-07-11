ad7f63b21c54509440de5d8712328452c63de858
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-11 23:39:23 +0300

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -41,7 +41,7 @@ module.exports = (client, guild) => {
 
        const embed = new MessageEmbed()
       .setTitle("WHY!!")
-      .setDescription(`"i sended this message to you cause you're the guild owner!**")
+      .setDescription("**i sended this message to you cause you're the guild owner!**")
       .setColor(Color)     
       .addField("Kicked From:", `\`${guild.name}\``)
       .addField("Invite Me:", `To ${guild.name} [With Clicking Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands)`
