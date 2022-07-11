efaee00756a3bac836ae1c374c782ea111c8f2ec
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-11 14:36:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose.connect(config.database2 ,{ useNewUrlParser: true, useUnifiedTopology: 
 
 
 client.commands = new Discord.Collection();
-const Categories = ["moderation", "public", "owner", "fun", "gif", "text"];
+const Categories = ["moderation", "public", "owner", "fun", "gif", "text", "emote"];
 
 Categories.forEach(async function(Category, message) {
 fs.readdir(`./commands/${Category}`, (err, files) => {
