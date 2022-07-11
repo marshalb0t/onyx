5e4cc09e7144ef4da9f355b86822cf6a2914d02a
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-11 15:14:05 +0300

    Update jumbo.js

--- commands/emote/jumbo.js
+++ commands/emote/jumbo.js
@@ -7,7 +7,7 @@ module.exports = {
   run: async (client, message, args) => {
     if (!args[0]) return message.channel.send("Emote is a required argument that is missing.");
     let emo;
-     try { emo = bot.emojis.cache.get(args[0].match(/(?<=<?a?:?\w{2,32}:)\d{17,19}(?=>?)/gi)[0]) || discord.Util.parseEmoji(args[0]) || bot.emojis.cache.find(e => e.name === args[0]) || bot.emojis.cache.find(e => e.id === args[0]);
+     try { emo = client.emojis.cache.get(args[0].match(/(?<=<?a?:?\w{2,32}:)\d{17,19}(?=>?)/gi)[0]) || discord.Util.parseEmoji(args[0]) || client.emojis.cache.find(e => e.name === args[0]) || client.emojis.cache.find(e => e.id === args[0]);
      } catch { 
        return message.channel.send("ERROR")
      }
