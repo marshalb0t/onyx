ec8135584b9611d1f5a14f0921b312d1ba2b6cb4
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-12 10:10:44 +0300

    Update unlock.js

--- commands/moderation/unlock.js
+++ commands/moderation/unlock.js
@@ -1,37 +1,49 @@
-const Discord = require("discord.js");
-const language = "en";
-const db = require("quick.db")
+const { MessageEmbed } = require("discord.js")
+const { color } = require("../../config.json");
+
 module.exports = {
-    name: "unlock",
-    description: "to unhide channel",
-    permissions: 'MANAGE_CHANNELS',
-    run: async (client, message, args, db) => { 
- 
+  name: "unlock",
+  aliases: ["unlockchannel"],
+  description: "unlocking one of server channels",
+  usage: "unlock",
+  run: async (client, message, args) => {
 
-    if (!message.channel.guild){
-            let dm = new Discord.MessageEmbed()
-           .setAuthor(`${message.author.username}`, `${message.author.avatarURL({dynamic:true})}`)
-          .setDescription("this command is only for servers.")
-          .setColor("9e1c36")
+        if(!message.member.hasPermission('MANAGE_CHANNELS')) {
+            const lockchannelError = new MessageEmbed()
+            .setDescription('You don\'t have permission to unlock channels!')
+            .setColor(color)
 
-return message.channel.send(dm)
+            return message.channel.send(lockchannelError)
+        }
 
+        let channel = message.mentions.channels.first();
+        let reason = args.join(" ") || 'Not Specified'
+
+        if(channel) {
+            reason = args.join(" ").slice(22) || 'Not Specified'
+        } else (
+            channel = message.channel
+        )
+
+        if(channel.permissionsFor(message.guild.id).has('SEND_MESSAGES') === true) {
+            const lockchannelError2 = new MessageEmbed()
+            .setDescription(`${channel} is already Unlocked!`)
+            .setColor(color)
+
+            return message.channel.send(lockchannelError2)
+        }
+
+        channel.updateOverwrite(message.guild.id, { SEND_MESSAGES: true })
+
+        const embed = new MessageEmbed()
+        .setTitle(`Channel UnLocked!`)
+        .addField("UnLocked:", `${channel}`, true)
+        .addField("UnLocker:", `${message.author.tag}`, true)
+        .addField("Reason:", `${reason}`, true)
+        .setColor(color)
+        .setTimestamp()   
+        message.react("🔓")      
+        message.channel.send(embed);
+
+    }
 }
-
-
-  
-
-    message.channel
-      .createOverwrite(message.guild.id, {
-        SEND_MESSAGES: true
-      })
-      .then(() => {
-       let embedd = new Discord.MessageEmbed()
-      .setAuthor(`${message.author.username}`, `${message.author.avatarURL({dynamic:true})}`)
-      .setDescription("Done its unlocked.")
-      .setColor("9e1c36")  
-
-return message.channel.send(embedd)
-  });
-  }}
-
