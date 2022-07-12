b1acde4939f6fc53a01a5de82cc0cbf9fdbc8be2
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-12 10:26:02 +0300

    Create errors.js

---
+++ events/errors.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.run = async (client, message) => {
+    console.error()
+    const errorch = '994335174005366785'
+    const channel = client.channels.cache.get(errorch);
+    const errormsg = new MessageEmbed()
+    .setTitle('an error occured')
+    .setDescription(`**${error}**`)
+    channel.send(errormsg)
+}
