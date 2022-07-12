65e791e25e7927622f0b59d31376bc6d617617d3
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-12 10:14:35 +0300

    Update hide.js

--- commands/moderation/hide.js
+++ commands/moderation/hide.js
@@ -1,41 +1,49 @@
+const { MessageEmbed } = require("discord.js")
+const { color } = require("../../config.json");
 
-//// كود اخفاء و اضهار الروم
-const Discord = require("discord.js");
-const language = "en" 
-const db = require("quick.db")
 module.exports = {
-    name: "hide",
-    aliases: ['اخفي'],
-    description: "hide server channels",
-    permissions: 'MANAGE_CHANNELS',
-    run: async (client, message, args) => {  
+  name: "hide",
+  aliases: ["hidechannel"],
+  description: "unlocking one of server channels",
+  usage: "unlock",
+  run: async (client, message, args) => {
 
-  
+        if(!message.member.hasPermission('MANAGE_CHANNELS')) {
+            const lockchannelError = new MessageEmbed()
+            .setDescription('You don\'t have permission to hide channels!')
+            .setColor(color)
 
-  
-  
-    if (!message.channel.guild) {
-      let dm = new Discord.MessageEmbed()
-      .setAuthor(`${message.author.username}`, `${message.author.avatarURL({dynamic:true})}`)
+            return message.channel.send(lockchannelError)
+        }
 
-          .setDescription("this command is only for servers.")
-          .setColor("9e1c36")
-          return message.reply(dm)
-      
-      
+        let channel = message.mentions.channels.first();
+        let reason = args.join(" ") || 'Not Specified'
+
+        if(channel) {
+            reason = args.join(" ").slice(22) || 'Not Specified'
+        } else (
+            channel = message.channel
+        )
+
+        if(channel.permissionsFor(message.guild.id).has('VIEW_CHANNEL') === false) {
+            const lockchannelError2 = new MessageEmbed()
+            .setDescription(`${channel} is already Hided!`)
+            .setColor(color)
+
+            return message.channel.send(lockchannelError2)
+        }
+
+        channel.updateOverwrite(message.guild.id, { VIEW_CHANNEL: false })
+
+        const embed = new MessageEmbed()
+        .setTitle(`Channel Hided!`)
+        .addField("Hided:", `${channel}`, true)
+        .addField("Hider:", `${message.author.tag}`, true)
+        .addField("Reason:", `${reason}`, true)
+        .setColor(color)
+        .setTimestamp()   
+        message.react("")      
+        message.channel.send(embed);
+
     }
-
-    
-    message.channel
-message.channel.createOverwrite(message.guild.id, {
-  VIEW_CHANNEL: false
-})
-      .then(() => {
-      let embedd = new Discord.MessageEmbed()
-      .setAuthor(`${message.author.username}`, `${message.author.avatarURL({dynamic:true})}`)
-
-      .setDescription("Done its hidden.")
-      .setColor("9e1c36")  
-        return message.channel.send(embedd);
-        });
-  }}
+}
