2b83ee8f072bdfaa8c61175aa74989b759d57347
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-13 16:10:00 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -9,29 +9,6 @@ const { Client, Guild, MessageEmbed } = require("discord.js")
 
 module.exports = (client, guild) => {
 
-   const owner = new MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle("Hello, I'm Run Beta!")
-      .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-      .addField(
-        "Information",
-        `i just joined to your server **${guild.name}**.\n\nHere is some information about myself:\n\nMy Prefix:\`.help\`\nCommands: Moderation, misc, info, fun, utility`,
-        true
-      )
-      .addField(
-        "Invite me",
-        "Add Me To Your Server By [click here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)",
-        true
-      )
-      .addField(
-        "Elop Bot",
-        "Use me and enjoy!",
-        true
-      )
-      .setTimestamp() // moment().format('LLL'),
-      .setFooter(`Enjoy`);
-   guild.owner.send(owner);
-
  let channel = client.channels.cache.get("994933719263608922");
 
  const joinEmbed = new MessageEmbed()
@@ -39,17 +16,17 @@ module.exports = (client, guild) => {
        .setTitle("New Server!!")
        .setThumbnail(guild.iconURL())
        .addField(
-         "Information",
+         "Guild:",
          `i just joined to here **${guild.name}**.\n\nThere is **${guild.memberCount}** members`,
          true
        )
        .addField(
-         "Owner",
-         `${guild.owner.user.tag} / ${guild.ownerID}`,
+         "Owner:",
+         `${guild.owner}`,
          true
        )
        .addField(
-         "Servers",
+         "Servers:",
          `[${client.guilds.cache.size}]`,
          true
        )
