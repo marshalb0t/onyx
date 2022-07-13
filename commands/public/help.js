e4876668dbfc83c5760791ddc93bd4b58afca9ae
Nex <87871568+dev-silent@users.noreply.github.com>
2022-07-13 23:47:08 +0300

    Update help.js

--- commands/public/help.js
+++ commands/public/help.js
@@ -73,13 +73,13 @@ module.exports = {
         .setFooter('6th Page', client.user.displayAvatarURL({ dynamic: true}))
         .setTimestamp()
       
-       let txt = ["textkurdish", "textarabic", "textenglish", "textpersian", "textturkish"]
+       let txt = ["textkurdish", "textarabic", "textpersian", "textturkish"]
   
         const embed6 = new Discord.MessageEmbed()
          .setTitle("❯ Text Category:")
         .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-        .addField("Availby Commands: "+`[${txt.length}]`, `\`textkurdish, textenglish, textarabic, textpersian, textturkish\``)
-        .addField("Aliases:", `\`tk, te, ta, tp, tt\``)
+        .addField("Availby Commands: "+`[${txt.length}]`, `\`textkurdish, textarabic, textpersian, textturkish\``)
+        .addField("Aliases:", `\`tk, ta, tp, tt\``)
         .setColor(Color)
         .setFooter('7th Page', client.user.displayAvatarURL({ dynamic: true}))
         .setTimestamp()
